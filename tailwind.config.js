462100aa1b43ac194fbc4189bcd692e893d5ae2f
Gustavo Carvalhaes <gustavocsak@gmail.com>
2023-03-04 18:27:56 -0800

    Added partial navbar work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'opensans': ['Open Sans', 'sans-serif'],
+        'sans': ['Inter var', ...defaultTheme.fontFamily.sans],
+    }
   },
   plugins: [],
+  darkMode: 'class'
 }
