de98fdc89c625c5de0dc0d3ed134e67efbe678a0
Gustavo Carvalhaes <gustavocsak@gmail.com>
2023-03-08 20:43:27 -0800

    Changed navbar and hero section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import styles from "./index.css";
 import Navbar from "./components/Navbar";
-import Blob from "./components/Blob";
 
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="bg-[#0E1129] px-10 text-white">
-            <section className="min-h-screen relative">
+        <main className="bg-white px-10">
+            <section className="min-h-screen">
                 <Navbar />
-                
-                <Blob />
             </section>
-            
-        </div>
+        </main>
       );
     
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-
+import { BsFillMoonStarsFill } from 'react-icons/bs'
 
 export default function Navbar() {
   return (
-    <nav className='py-8 flex justify-between'>
-        <h1 className='text-xl font-semibold'>gustavo</h1>
-        <ul className='flex items-center'>
+    <nav className="py-10 mb-12 flex justify-between">
+        <h1 className="text-xl">gustavo</h1>
+        <ul className="flex items-center">
             <li>
-                <a className='bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-2 text-white rounded-md ml-8' href='#'>
-                    Resume
-                </a>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+            </li>
+            <li>
+                <a href="#" className="bg-gradient-to-r from-sky-500 to-cyan-500 text-white px-4 py-2 rounded-md ml-8">Resume</a>
             </li>
         </ul>
     </nav>
