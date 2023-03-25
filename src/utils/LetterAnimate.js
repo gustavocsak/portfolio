c114fbd6df37b22f420a57e418c46d8ffeb2e88f
Gustavo Carvalhaes <gustavocsak@gmail.com>
2023-03-25 13:45:05 -0700

    Added animation to title

--- src/utils/LetterAnimate.js
+++ src/utils/LetterAnimate.js
@@ -2,15 +2,25 @@ import React from "react";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-const LetterAnimate = ({ letter }) => {
+const LetterAnimate = ({ letter, time }) => {
   return (
     <motion.span
+      
+      animate={{
+        color: ["rgb(255,255,255)", "rgb(193, 51, 255)", "rgb(255,255,255)"],
+        transition: {
+            repeat: Infinity,
+            duration: 5
+        }
+      }}
+      
       whileHover={{
         color: "rgb(193, 51, 255)",
         transition: {
-           
-            duration: 0.2
-        }
+            
+            duration: 0.1
+        },
+       
       }}
     >
       {letter}
