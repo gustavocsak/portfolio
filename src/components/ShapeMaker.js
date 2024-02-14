776fb4641ac210b086133f20960c8d65304f6552
gustavocsak <gustavocsak@gmail.com>
2024-02-13 23:08:13 -0800

    feat: add init engine for tsparticles

--- src/components/ShapeMaker.js
+++ src/components/ShapeMaker.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { loadSlim } from '@tsparticles/slim';
 
 const ShapeMaker = ({ color }) => {
+
+	const [init, setInit] = useState(false);
+
+	useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			await loadSlim(engine);
+		}).then(() => {
+			setInit(true);
+		});
+	}, []);
+
 	return (
 		<div
 			className='h-full border-4 rounded-xl'
 			style={{ borderColor: color }}
 		>
-			
+
 		</div>
 	)
 }
