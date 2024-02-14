93da65de8ce3c650adabf7129b0a49bc8bd66dff
gustavocsak <gustavocsak@gmail.com>
2024-02-13 23:29:39 -0800

    feat: add p5js to project

--- src/components/ShapeMaker.js
+++ src/components/ShapeMaker.js
@@ -1,26 +1,11 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import Particles, { initParticlesEngine } from '@tsparticles/react'
-import { loadSlim } from '@tsparticles/slim';
+import React from 'react'
+
 
 const ShapeMaker = ({ color }) => {
 
-	const [init, setInit] = useState(false);
-
-	useEffect(() => {
-		initParticlesEngine(async (engine) => {
-			await loadSlim(engine);
-		}).then(() => {
-			setInit(true);
-		});
-	}, []);
-
+	
 	return (
-		<div
-			className='h-full border-4 rounded-xl'
-			style={{ borderColor: color }}
-		>
-
-		</div>
+		<></>
 	)
 }
 
