fe566412dad5b7ba815e0176dcfa56a04d63df7f
gustavocsak <gustavocsak@gmail.com>
2024-02-14 21:03:42 -0800

    feat: canvas hidden in mobile and resizes on bigger screens

--- src/components/ShapeMaker.js
+++ src/components/ShapeMaker.js
@@ -6,15 +6,17 @@ let y = 50;
 
 const ShapeMaker = ({ color }) => {
 	let divWidth;
+
+
 	const setup = (p5, canvasParentRef) => {
 		const divElement = document.getElementById('canvas');
-		if(divElement) {
+		if (divElement) {
 			divWidth = divElement.offsetWidth;
 		} else {
 			console.error('Element with canvas ID not found')
 		}
 		p5.createCanvas(divWidth, 500).parent(canvasParentRef);
-		
+
 	};
 
 	const mousePressed = (p5, event) => {
@@ -22,14 +24,30 @@ const ShapeMaker = ({ color }) => {
 		p5.ellipse(event.offsetX, event.offsetY, 70, 70);
 	}
 
+	const windowResized = (p5, event) => {
+		const divElement = document.getElementById('canvas');
+		if (divElement) {
+			divWidth = divElement.offsetWidth;
+		} else {
+			console.error('Element with canvas ID not found')
+		}
+		p5.resizeCanvas(divWidth, 500)
+	}
+
 	const draw = (p5) => {
 		// p5.background(0)
 		p5.ellipse(x, y, 70, 70);
-		x++;
+		x+=3;
 	};
 
 	return (
-		<Sketch setup={setup} draw={draw} mousePressed={mousePressed}/>
+		<Sketch
+			className='hidden lg:block'
+			setup={setup}
+			draw={draw}
+			mousePressed={mousePressed}
+			windowResized={windowResized}
+		/>
 	)
 }
 
